e5b35a053ebaa3dc890d71e43f6e9d88dd63cd7e
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-04-27 00:40:49 +0530

    Refactored: TC with exception handling and used command and query seperation principle

--- src/main/java/com/blz/ParkingLot.java
+++ src/main/java/com/blz/ParkingLot.java
@@ -1,22 +1,53 @@
 package com.blz;
 
 public class ParkingLot {
-    Vehicle vehicle;
+    private Vehicle vehicle;
 
-    public boolean park(Vehicle vehicle) {
-        if (this.vehicle != null)
-            return false;
-        this.vehicle = vehicle;
-        return true;
+    //default constructor
+    public ParkingLot() {
     }
 
-    public boolean unPark(Vehicle vehicle) {
+    /**
+     * @Purpose : To park the vehicle
+     * @Param : vehicle
+     */
+
+    public void vehicleParking(Vehicle vehicle) throws ParkingLotException {
+        if (this.vehicle != null)
+            throw new ParkingLotException("Parking Lot is Full");
+        this.vehicle = vehicle;
+    }
+    /**
+     * @Purpose : To unPark the vehicle
+     * @Param : vehicle
+     */
+    public void vehicleUnparking(Vehicle vehicle) throws ParkingLotException {
         if (this.vehicle == null)
-            return false;
-        if (this.vehicle.equals(vehicle)) {
-            this.vehicle=null;
-            return true;
+            throw new ParkingLotException("Lot is empty");
+        if (this.vehicle .equals(vehicle)){
+            this.vehicle = null;
+            return;
         }
+        throw new ParkingLotException("Please ask correct vehicle");
+    }
+    /**
+     * @Purpose : Method to check vehicle is park or not
+     * @Param : vehicle
+     * @Return : Returns boolean value true or false
+     */
+    public boolean isParked(Vehicle vehicle) {
+        if (this.vehicle.equals(vehicle))
+            return true;
+        return false;
+    }
+    /**
+     * @Purpose :  Method to check vehicle is unpark or not
+     * @Param : vehicle
+     * @Return : Returns boolean value true or false
+     */
+    public boolean isUnParked(Vehicle vehicle) {
+        if (this.vehicle.equals(vehicle))
+            return true;
         return false;
     }
 }
--- src/test/java/com/blz/ParkingLotTest.java
+++ src/test/java/com/blz/ParkingLotTest.java
@@ -6,26 +6,91 @@ import org.junit.Test;
 
 public class ParkingLotTest {
 
+    ParkingLot parkingLot = null;
     @Before
     public void setUp() throws Exception {
-        ParkingLot parkingLot = new ParkingLot();
+        parkingLot = new ParkingLot();
     }
 
-    //UC1 = Ability To Park vehicle
+    /**
+     *TC1 = Ability To Park vehicle
+     *Given vehicle when parked should return true
+     */
     @Test
     public void givenVehicle_WhenParked_ShouldReturnTrue() {
         Vehicle vehicle = new Vehicle("alto");
-        ParkingLot parkingLot;
-        boolean isParked = parkingLot.park(vehicle);
-        Assert.assertTrue(isParked);
-    }
+        try {
+            parkingLot.vehicleParking(vehicle);
+            boolean isParked = parkingLot.isParked(vehicle);
+            Assert.assertTrue(isParked);
+        } catch (ParkingLotException e) {
+            e.printStackTrace();
+        }
 
-    //UC : Ability to unpark Vehicle
+    }
+    /**
+     * TC2 = Ability To Park vehicle
+     * given a vehicle when already parked should throw exception
+     */
     @Test
-    public void givenVehicle_WhenUnparked_ShouldReturnTrue() {
+    public void givenVehicle_WhenAlreadyParked_ShouldThrowException() {
         Vehicle vehicle = new Vehicle("alto");
-        parkingLot.park(vehicle);
-        boolean isUnParked = parkingLot.unPark(vehicle);
-        Assert.assertTrue(isUnParked);
+
+        try {
+            parkingLot.vehicleParking(vehicle);
+            parkingLot.vehicleParking(new Vehicle("alto 80"));
+            boolean isParked = parkingLot.isParked(vehicle);
+        } catch (ParkingLotException e) {
+            Assert.assertEquals("Parking Lot is Full", e.getMessage());
+            e.printStackTrace();
+        }
+    }
+    /**
+     * TC3 = Ability To unPark vehicle
+     * Given a vehicle when unparked should return true
+     */
+    @Test
+    public void givenVehicle_WhenUnParked_ShouldReturnTrue() {
+        Vehicle vehicle = new Vehicle("alto");
+        try {
+            parkingLot.vehicleParking(vehicle);
+            boolean isUnParked = parkingLot.isUnParked(vehicle);
+            Assert.assertTrue(isUnParked);
+        } catch (ParkingLotException e) {
+            e.printStackTrace();
+        }
+
+    }
+    /**
+     * TC4 = Ability To unPark vehicle
+     * given a unparked vehicle when try to unpark should return throw exception
+     */
+    @Test
+    public void givenUnParkedVehicle_WhenTryToUnPark_ShouldReturnThrowException() {
+        Vehicle vehicle = new Vehicle("alto");
+        try {
+            parkingLot.vehicleUnparking(vehicle);
+        } catch (ParkingLotException e) {
+            Assert.assertEquals("Lot is empty", e.getMessage());
+            e.printStackTrace();
+        }
+
+    }
+    /**
+     * TC5 = Ability To unPark vehicle
+     * Given a vehicle when try to unpark different vehicle should throw exception
+     */
+    @Test
+    public void givenVehicle_WhenTryToUnParkDifferentVehicle_ShouldThrowException() {
+        Vehicle vehicle = new Vehicle("alto");
+        Vehicle vehicle1 = new Vehicle("alto 80");
+        try {
+            parkingLot.vehicleParking(vehicle);
+            parkingLot.vehicleUnparking(vehicle1);
+        } catch (ParkingLotException e) {
+            Assert.assertEquals("Please ask correct vehicle", e.getMessage());
+            e.printStackTrace();
+        }
+
     }
 }
